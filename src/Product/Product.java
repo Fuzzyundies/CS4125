998c3a0fe6ada2a2792e371d657057f52f260215
Benjamin Grimes <benjamingrimes182@gmail.com>
2018-10-28 17:54:16 +0000

    Added Product package, Product class and Category class.

---
+++ src/Product/Product.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Product;
+
+import User.Customer;
+
+/**
+ *
+ * @author Benjamin Grimes
+ */
+public class Product 
+{
+    private String id;
+    private String name;
+    private Category category;
+    private Customer owner;
+    private double price;
+    private int rating;
+    private String description;
+    private String URLPic;
+    
+    
+}
