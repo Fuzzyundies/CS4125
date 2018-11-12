b9b054b7f12a6253785d6cd209f8accf3ba47128
Neil <neildaly2142@gmail.com>
2018-11-12 22:55:44 +0000

    Admin test using Factory

---
+++ test/Business/User/AdminTest.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.User;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author neild
+ */
+public class AdminTest {
+
+    private UserFactory userFactory = null;
+    private IUser user;
+
+    public AdminTest() {
+        userFactory = new UserFactory();
+    }
+
+    @Test
+    public void testResetPassword() {
+    }
+
+    @Test
+    public void testDeleteProduct() {
+    }
+
+    @Test
+    public void testDeleteAccount() {
+    }
+
+    @Test
+    public void testSearchHistory() {
+    }
+
+    @Test
+    public void testGetID() {
+        user = userFactory.createUser("A");
+        assertEquals("101", user.getID());
+    }
+
+    @Test
+    public void testGetEmail() {
+        //Purposely fail this test
+        user = userFactory.createUser("A");
+        assertEquals("someEmail@mail.ie", user.getEmail());
+    }
+
+    @Test
+    public void testGetName() {
+    }
+
+    @Test
+    public void testGetPassword() {
+    }
+
+    @Test
+    public void testSetID() {
+    }
+
+    @Test
+    public void testSetEmail() {
+    }
+
+    @Test
+    public void testSetName() {
+    }
+
+    @Test
+    public void testSetPassword() {
+    }
+
+    @Test
+    public void testUpdate() {
+    }
+
+}
