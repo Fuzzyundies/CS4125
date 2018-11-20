a2b3f37838de1d07410e7f64f19c72d69940f878
Neil <neildaly2142@gmail.com>
2018-11-20 20:34:32 +0000

    Creating some product states

---
+++ src/Business/Product/ProductReturned.java
@@ -0,0 +1,24 @@
+package Business.Product;
+
+/**
+ *
+ * @author neild
+ */
+public class ProductReturned implements ProductState {
+
+    @Override
+    public void nextState() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void previousState() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getProductStatus() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
---
+++ src/Business/Product/ProductTransit.java
@@ -0,0 +1,26 @@
+
+
+package Business.Product;
+
+/**
+ *
+ * @author neild
+ */
+public class ProductTransit implements ProductState {
+
+    @Override
+    public void nextState() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void previousState() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getProductStatus() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
